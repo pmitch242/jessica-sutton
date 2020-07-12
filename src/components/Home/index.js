0bdb43f5a06a5e46f701023a6783ebf824c7463c
Phill <phillicia.mitchell21@gmail.com>
2020-07-12 18:34:28 -0400

    adding grids

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,22 +1,45 @@
 import React from 'react'
-import {  } from '@material-ui/core';
+import { Grid, Paper, makeStyles, Container } from '@material-ui/core';
 
 import Intro from './Intro';
 
 import './home.css';
 
-// const useStyle = makeStyles({
-//     home: {}
-// })
+const useStyle = makeStyles({
+    home: {}
+})
 
 const Home = () => {
-        return (
-            <div id='home'>
-                <Intro />
-                <section id='landing'></section>
-                <section id='about-section'></section>
-            </div>
-        )
+    const classes = useStyle();
+
+    return (
+        <Container id='home'>
+            <Grid container spacing={1}>
+
+                <Grid container item xs={12} spacing={3}>
+                    <Grid container item xs={12}>
+                        <Grid item xs={12}><Intro /></Grid>
+                    </Grid>
+                </Grid>
+
+                
+                <Grid container item xs={12} spacing={3}>
+                    <Grid container item xs={12}>
+                        <Grid item xs={12}><Paper className={classes.paper}>item</Paper></Grid>
+                    </Grid>
+                </Grid>
+
+                
+                <Grid container item xs={12} spacing={3}>
+                    <Grid container item xs={12}>
+                        <Grid item xs={12}><Paper className={classes.paper}>item</Paper></Grid>
+                    </Grid>
+                </Grid>
+
+                
+            </Grid>
+        </Container>
+    )
 }
 
 export default Home;

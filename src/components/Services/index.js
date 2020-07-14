514539ab2426bb262c65837b56634da16d003a31
Phill <phillicia.mitchell21@gmail.com>
2020-07-14 17:23:10 -0400

    added extra info card

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles, Typography, Container, Divider, Grid } from '@material-ui/core';
+import { makeStyles, Typography, Container, Divider, Grid, Card, CardContent } from '@material-ui/core';
 
 import thiefSVG from '../../icons/svg/003-thief.svg';
 import accidentIcon from '../../icons/svg/004-accident.svg';
@@ -11,14 +11,18 @@ import ServiceCard from './ServiceCard';
 const useStyle = makeStyles({
     root: {
         backgroundColor: '#f9f9f9',
-        paddingTop: '100px',
+        paddingTop: '125px',
         paddingBottom: '100px',
     },
     conatiner: {
         margin: '50px auto'
     },
     h2: {
-        marginBottom: '10px',
+        marginBottom: '20px',
+    },
+    cardH2: {
+        marginBottom: '20px',
+        color: '#e9e9e9'
 
     },
     divider: {
@@ -30,7 +34,22 @@ const useStyle = makeStyles({
     icon: {
         fontSize: '5rem',
         margin: '20px'
-    }
+    },
+    extraCard: {
+        backgroundColor: '#001639',
+        color: '#c5c5c5',
+        lineHeight: '20px',
+        textAlign: 'center',
+        height: '100%',
+    },
+    extraCardContent: {
+        width: '60%',
+        margin: 'auto',
+    },
+    cardDivider: {
+        marginBottom: '20px',
+        backgroundColor: '#d8973c'
+    },
 })
 
 const Services = () => {
@@ -71,6 +90,17 @@ const Services = () => {
                             icon={sendIcon}
                             link='/'
                         />
+                    </Grid>
+                    <Grid item xs={12} sm={8}>
+                        <Card className={classes.extraCard}>
+                            <CardContent className={classes.extraCardContent}>
+                                <Typography variant='h2' className={classes.cardH2}>Services</Typography>
+                                <Divider color='secondary' className={classes.cardDivider}/>
+                                <Typography variant='body1'>
+                                    We are highly experienced, thoughtful in our role as advocates, and committed to being responsive to what our clients need and demand.
+                                </Typography>
+                            </CardContent>
+                        </Card>
                     </Grid>
                 </Grid>
             </Container>

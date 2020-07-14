4aa7d854c30f7fb16d06b26f4f797c27866c4074
Phill <phillicia.mitchell21@gmail.com>
2020-07-14 11:37:27 -0400

    set up grid for contact section

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,24 +1,56 @@
 import React from 'react';
-import { makeStyles, Typography } from '@material-ui/core';
+import { makeStyles, Typography, Container, Divider, Grid } from '@material-ui/core';
+
+import ServiceCard from '../Services/ServiceCard';
 
 const useStyle = makeStyles({
-    container: {
-        maxWidth: '600px',
-        marginTop: '10%',
-        marginBottom: '20%',
-        color: '#e9e9e9',
+    root: {
+        backgroundColor: '#e9e9e9',
+        paddingTop: '100px',
+        paddingBottom: '100px',
     },
+    h2: {
+        marginBottom: '10px',
+
+    },
+    divider: {
+        marginBottom: '30px',
+        maxWidth: '250px',
+        height: '3px',
+        backgroundColor: '#d8973c'
+    },
+    contactCard:{},
 })
 
 const Contact = () => {
     const classes = useStyle();
 
     return (
-            <div id='about' className={classes.container}>
-                <Typography variant='h1' >
-                    Fugiat dolor laboris culpa ad labore
-                </Typography>
-            </div>
+        <div id='about' className={classes.root}>
+            <Container >
+                <Typography variant='h2' color='secondary' className={classes.h2}>Get Free Consultation</Typography>
+
+                <Divider className={classes.divider} />
+
+                <Grid container spacing={4} className={classes.container}>
+                    <Grid item xs={12} sm={6}>
+                        <ServiceCard />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <div id='contact-card' className={classes.contactCard}>
+                            <ServiceCard />
+                        </div>
+                        <div id='contact-notice'>
+                            <Typography variant='h3'>Notice</Typography>
+                            <Typography variant='subtitle1'>
+                                *For legal purposes, please do not include any sensitive,
+                                private or confidential information in this online form.
+                            </Typography>
+                        </div>
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>
     )
 }
 

90f8dcb60cdb4a8b877e9a83e7ada4784c23bb3b
Phill <phillicia.mitchell21@gmail.com>
2020-07-27 14:28:44 -0400

    some styling

---
+++ src/components/About/CountingNumbers.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+import { makeStyles, Box } from '@material-ui/core'
+
+const useStyle = makeStyles({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+        width: '100%',
+        backgroundColor: '#001639',
+        padding: '15px 0px',
+        borderRadius: '20px',
+        color: '#fff',
+        fontFamily: `'Noto Sans TC', sans-serif`,
+        lineHeight: '20px'
+    },
+    number: {
+        fontSize: '2rem',
+        marginRight: '2px',
+        fontWeight: 'bolder',
+    },
+    plus: {
+        fontSize: '1.75rem',
+        position: 'absolute',
+        color: '#d8973c',
+        fontWeight: '900',
+    },
+    title: {
+        fontSize: '1.5rem',
+        textTransform: 'capitalize',
+        fontWeight: '400',
+        letterSpacing: '1px',
+        
+    },
+})
+const CountingNumbers = () => {
+    const classes = useStyle();
+
+    return (
+        <Box id='counting-numbers' className={classes.root}>
+            <div style={{marginBottom: '10px'}}>
+                <span className={classes.number}>35</span>
+                 <span className={classes.plus}>+</span>
+            </div>
+            <span className={classes.title}>Clients</span>
+        </Box>
+    )
+}
+
+export default CountingNumbers
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { makeStyles, Typography, Container, Divider, Grid, Box } from '@material-ui/core';
 
+import CountingNumbers from './CountingNumbers';
 import jess from '../../images/jess.jpg';
 
-import ServiceCard from '../Services/ServiceCard';
-
+import './about.css';
 
 const useStyle = makeStyles({
     root: {
@@ -13,13 +13,12 @@ const useStyle = makeStyles({
     },
     h2: {
         marginBottom: '10px',
-
     },
     divider: {
         marginBottom: '30px',
         maxWidth: '250px',
         height: '3px',
-        backgroundColor: '#d8973c'
+        backgroundColor: '#d8973c',
     },
     container: {
         // backgroundColor: '#f9f9f9',
@@ -47,17 +46,21 @@ const About = () => {
     return (
         <div id='about' className={classes.root}>
             <Container >
+                <Typography variant='h2' color='primary' className={classes.h2}>Jessica Sutton</Typography>
+                <Divider className={classes.divider} />
 
 
                 <Grid container spacing={3} className={classes.container}>
                     <Grid item xs={12} sm={5}>
-                        <img alt='Jessica Sutton' src={jess} className={`jessImg ${classes.img}`} />
+                        <div>
+                            <div className='jess-img-div'>
+                                <img alt='Jessica Sutton' src={jess} className={` ${classes.img}`} />
+                            </div>
+                        </div>
                     </Grid>
 
                     <Grid item xs={12} sm={7} >
                         <Box>
-                            <Typography variant='h2' color='primary' className={classes.h2}>Jessica Sutton</Typography>
-                            <Divider className={classes.divider} />
 
                             <Typography variant='body1' className={classes.body} color='primary'>
                                 Jessica Sutton has over a decade of experience in representing death-eligible and death-sentenced clients as both an attorney and a mitigation specialist. In her role as an attorney, Ms. Sutton has represented clients in state and federal proceedings, including state post-conviction and federal habeas corpus proceedings, lethal injection litigation, Section 1983 civil rights lawsuits, and clemency.
@@ -70,15 +73,17 @@ const About = () => {
                                 In addition to capital work, Ms. Sutton regularly conducts mitigation investigations in felony sentencings, parole cases, and cases petitioning for sentence modifications for individuals sentenced to life as juveniles. She also travels around the country to present at conferences concerning mitigation investigation and frequently trains and consults with teams nationwide.
                         </Typography>
 
-                            <Grid container spacing={1} >
+                            <Grid container spacing={10} >
                                 <Grid item xs={12} sm={4}>
-                                    <ServiceCard />
+                                    <CountingNumbers number='' title=''/>
                                 </Grid>
+
                                 <Grid item xs={12} sm={4}>
-                                    <ServiceCard />
+                                    <CountingNumbers />
                                 </Grid>
+
                                 <Grid item xs={12} sm={4}>
-                                    <ServiceCard />
+                                    <CountingNumbers />
                                 </Grid>
                             </Grid>
                         </Box>

8e2853e6ba9b982069c8d15fdf5fb0c86de288b8
Phill <phillicia.mitchell21@gmail.com>
2020-08-24 23:24:07 -0400

    imported ServicesSection component and added elements to style

--- src/components/Services/Service.jsx
+++ src/components/Services/Service.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles, Button, Container, Paper } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
-const Service = (props) => {
-    return (
-        <div id={``}>
-            <div className="">
-                <h1>props.name</h1>
-                <p>For nearly a decade, Jessica Sutton has been providing case consultation services to individual defense teams, law firms, and federal and state defender agencies around the country, including agencies and teams in California, Washington D.C., Pennsylvania, Oklahoma, Missouri, Mississippi, and Texas, among others.</p>
+const Service = ({ servicesList }) => {
+
+    const services = servicesList.map(service =>
+        <div id='service-section'>
+            <div id={`${service.name + '-section'}`}>
+                <Container>
+                    <h1 className='h1'>{service.name}</h1>
+                    <p>{service.paragraph}</p>
+                    <Paper >
+                        <Container>
+                            <div className="card-details">
+                                <h1>{service.name} Areas</h1>
+                                <ul className='service-card-list'>
+                                    {service.list.map(item =>
+                                        <li>{item}</li>
+                                    )}
+                                </ul>
+                                <Link to='/contact' className='no-a'><Button variant="contained" color='secondary'>Request Consultation</Button></Link>
+                            </div>
+
+                        </Container>
+                    </Paper>
+                </Container>
             </div>
         </div>
+    )
+
+    return (
+            services
     )
 }
 
--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -12,16 +12,50 @@ const useStyle = makeStyles({
     },
 })
 
+const servicesList = [
+    {
+        name: 'consultation',
+        paragraph: 'For nearly a decade, Jessica Sutton has been providing case consultation services to individual defense teams, law firms, and federal and state defender agencies around the country, including agencies and teams in California, Washington D.C., Pennsylvania, Oklahoma, Missouri, Mississippi, and Texas, among others.',
+        list: [
+            'Trial strategy',
+            'Imitigation investigation',
+            'Developing life history narratives',
+            'Sentencing hearings',
+            'Parole proceedings',
+            'Juvenile re-sentencings',
+            'Compassionate release motions',
+            'Identifying and retaining experts',
+            'File organization and mitigation tools',
+            'CoVid-19 litigation',
+            'Funding litigation',
+        ],
+    },
+    {
+        name: 'training',
+        paragraph: `Jessica Sutton regularly teaches as faculty at regional and national conferences. Ms. Sutton also provides training to law firms and federal and state defender organizations. In addition to the topics above, Ms. Sutton offers broad training on the development, presentation, and litigation of certain types of cases, such as compassionate release, parole, and juvenile re-sentencings. Ms. Sutton's trainings are tailor-made to fit the needs of each client and range from hands-on exercises to informational presentations.`,
+        list: [
+            'Litigating CoVid-19 Issues',
+            'Investigating Brain Dysfunction and Cognitive Disorders',
+            'Trauma, Mental Health Issues, and Working with Experts',
+            'Using Social Media to Conduct Investigations',
+            'Conducting Effective Witness Interviews',
+            'Developing Mitigation Themes and Theories',
+            'Building and Managing Client Relationships',
+            'Introduction to Case Management ',
+        ],
+    },
+];
+
 const Services = () => {
     const classes = useStyle();
 
     return (
         <div id='services' className={classes.root}>
             {/* Header */}
-            <ServicesHeader name='Sevices Page'/>
+            <ServicesHeader name='Sevices Page' />
 
             {/* Service */}
-            <Service />
+            <Service servicesList={servicesList}/>
         </div>
     )
 }

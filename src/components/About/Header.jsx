a3f494db85af2d9e04c765b24161ce2a69302d5e
Phill <phillicia.mitchell21@gmail.com>
2020-08-25 18:51:30 -0400

    styled about page header

--- src/components/About/Header.jsx
+++ src/components/About/Header.jsx
@@ -1,33 +1,61 @@
 import React from 'react'
-import { Button } from '@material-ui/core'
+import { Button, Container, makeStyles } from '@material-ui/core'
+
+import jessicaImage from './jessica-image.png';
+
+const useStyle = makeStyles({
+    button: {
+        width: '100%',
+        margin: '30px auto',
+        color: '#001639',
+        letterSpacing: '0.75px',
+        fontWeight: 600,
+    }
+})
 
 const Header = () => {
+    const classes = useStyle();
+
     return (
         <div className='about-header-container'>
             <div className="about-header">
-                <div className="about-intro-div">
-                    <h1>Hey There, I'm Jessica!</h1>
+                <Container style={{ marginTop: '125px' }}>
+                    <div className="about-intro-div">
+                        <h1 className='h1'>Hey I'm <span>Jessica!</span></h1>
+                        <p>
+                            Approaching every matter with strategic thinking,
+                            creative elements and proven results.
+                    </p>
+                        <Button
+                            variant='contained'
+                            color='secondary'
+                            className={classes.button}
+                        >
+                            Let's Talk!
+                        </Button>
+                    </div>
 
-                    <p>Approaching every matter with strategic thinking, creative elements and proven results.</p>
-
-                    <Button variant='contained' color='secondary'>Let's Talk!</Button>
-                </div>
-                <div className="contact-info-div">
-                    <div className="contact-items">
-                        <div className="icon"></div>
-                        <div className="contact-items-content">
-                            <p className="p">Give Me a Call</p>
-                            <h3 className="h3">208 503 9381</h3>
+                    <div className="contact-info-div">
+                        <div className="contact-items">
+                            <div className="icon"></div>
+                            <div className="contact-items-content">
+                                <p className="p">Call Now</p>
+                                <h3 className="h3">208-503-9381</h3>
+                            </div>
+                        </div>
+                        <div className="contact-items">
+                            <div className="icon"></div>
+                            <div className="contact-items-content">
+                                <p className="p">Email Me</p>
+                                <h3 className="h3">jess.el.sutton@gmail.com</h3>
+                            </div>
                         </div>
                     </div>
-                    <div className="contact-items">
-                        <div className="icon"></div>
-                        <div className="contact-items-content">
-                            <p className="p">Email Me</p>
-                            <h3 className="h3">jess.el.sutton@gmail.com</h3>
-                        </div>
+
+                    <div className="about-image-div">
+                        <img src={jessicaImage} alt="Jessica" className="jessica-photo" />
                     </div>
-                </div>
+                </Container>
             </div>
         </div>
     )

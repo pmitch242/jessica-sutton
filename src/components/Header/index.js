c4c7bd4774a3007b746f71ed197f1bfc88714692
Phill <phillicia.mitchell21@gmail.com>
2020-08-26 12:06:49 -0400

    changed coloer of navbar to #f9f9f9 and adjusted navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,20 +7,9 @@ const Header = ({ name }) => {
 
     return (
         <div className='header-container'>
-            <div className='header-bg'></div>
             <div className="header">
                 <Container >
-                    <div className="intro-div">
-                        <h1 className='h1'>{name}</h1>
-                    </div>
-                </Container>
-            </div>
-
-            <div className="header-text-bg">
-                <Container >
-                    <div className="intro-div">
-                        <h1 className='h1'>{name}</h1>
-                    </div>
+                    <h1 className='h1'>{name}</h1>
                 </Container>
             </div>
         </div>

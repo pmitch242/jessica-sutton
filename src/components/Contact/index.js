5ce43580bc8276ba11bd0091cfd41cd4c869ebd3
Phill <phillicia.mitchell21@gmail.com>
2020-08-27 12:48:46 -0400

    added contact title information

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,6 +4,8 @@ import { makeStyles } from '@material-ui/core';
 import Header from '../Header';
 import ContactContent from './ContactContent';
 
+import './contact.css'
+
 const useStyle = makeStyles({
     root: {
         backgroundColor: '#e9e9e9',
